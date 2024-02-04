f9c70b5d8fffa0aac9a39f11ac2483e902588c1a
Yashwanth <yashwathsaikumar@gmail.com>
2024-02-04 17:01:14 +0530

    Sucess

--- typescript/src/App.js
+++ typescript/src/App.js
@@ -1,22 +1,16 @@
-import './App.css';
-import { Greet } from './Tutorials/AdvancedProps/Greet';
-import { Heading } from './Tutorials/AdvancedProps/Heading';
-import { Oscar } from './Tutorials/AdvancedProps/Oscar';
-import { Status } from './Tutorials/AdvancedProps/Status';
+import { Button } from "./EventProps/Button";
+import { Input } from "./EventProps/Input";
 
 
 function App() {
 
   return (
     <div className="App">
-      <Status status='loading' />
-      <Heading>Placeholder text </Heading>
-      <Oscar>
-        <Heading>
-          Oscar goes to Lenardo Dicpario!
-        </Heading>
-      </Oscar>
-      <Greet name='yash' isLoggedIn={true} />
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id)
+      }} />
+      <Input value='' handleChange={(event) => console.log(event)} />
+
 
 
     </div>
