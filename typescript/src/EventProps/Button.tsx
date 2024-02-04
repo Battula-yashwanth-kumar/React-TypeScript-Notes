552aaf6957d173a56d0dd247aec7b9ea8af8fa83
Yashwanth <yashwathsaikumar@gmail.com>
2024-02-04 17:02:02 +0530

    Event

--- typescript/src/EventProps/Button.tsx
+++ typescript/src/EventProps/Button.tsx
@@ -5,3 +5,26 @@ type ButtonProps = {
 export const Button = (props: ButtonProps) => {
     return <button onClick={(event) => props.handleClick(event, 1)}>Click</button>
 }
+ /*
+import { Button } from "./EventProps/Button";
+import { Input } from "./EventProps/Input";
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id)
+      }} />
+      <Input value='' handleChange={(event) => console.log(event)} />
+
+
+
+    </div>
+  );
+}
+
+export default App;
+
+ */
